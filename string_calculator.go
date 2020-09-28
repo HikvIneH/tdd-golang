c4e1f5f27bd4080b257449eca70ccf2f1582ccbc
Hikvineh <rarifky.aditya@gmail.com>
2020-09-28 11:19:09 +0700

    add test cases

--- string_calculator.go
+++ string_calculator.go
@@ -1,5 +1,35 @@
 package tdd_golang
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Add(input string) (int, error) {
-	return 0, nil
+	s := splitString(input)
+	n := 0
+	temp := 0
+
+	for _, v := range s {
+		temp, _ = strconv.Atoi(v)
+		if temp < 0 {
+			return temp, nil
+		}
+		n += temp
+	}
+	return n, nil
+}
+
+func splitString(input string) []string {
+	var s []string
+	if len(input) > 2 && input[0:2] == "//" {
+		s = strings.FieldsFunc(input[2:len(input)], Split)
+	} else {
+		s = strings.FieldsFunc(input, Split)
+	}
+	return s
+}
+
+func Split(r rune) bool {
+	return r == ',' || r == '\n' || r == ';'
 }
--- string_calculator_test.go
+++ string_calculator_test.go
@@ -1,8 +1,10 @@
 package tdd_golang_test
 
 import (
-	"github.com/supriyadi687/tdd-golang"
+	"fmt"
 	"testing"
+
+	tdd_golang "github.com/supriyadi687/tdd-golang"
 )
 
 func TestShouldReturn0GivenEmptyString(t *testing.T) {
@@ -10,4 +12,39 @@ func TestShouldReturn0GivenEmptyString(t *testing.T) {
 	if res != 0 {
 		t.Errorf("expected to be 0 got %d", res)
 	}
+}
+
+func TestShouldReturnOneGivenOneIntString(t *testing.T) {
+	res, _ := tdd_golang.Add("1")
+	if res != 1 {
+		t.Errorf("expected to be 1 got %d", res)
+	}
+}
+
+func TestShouldReturnSumGivenSumIntString(t *testing.T) {
+	res, _ := tdd_golang.Add("1,2")
+	if res != 3 {
+		t.Errorf("expected to be 3 got %d", res)
+	}
+}
+
+func TestShouldReturnSumGivenSumIntStringWithNewLine(t *testing.T) {
+	res, _ := tdd_golang.Add("1,2\n3")
+	if res != 6 {
+		t.Errorf("expected to be 6 got %d", res)
+	}
+}
+
+func TestShouldReturnSumGivenDifferentDelimiters(t *testing.T) {
+	res, _ := tdd_golang.Add("//;\n1;3")
+	fmt.Print(res)
+	if res != 4 {
+		t.Errorf("expected to be 4 got %d", res)
+	}
+}
+
+func TestShouldHandleNegativeNumber(t *testing.T) {
+	res, _ := tdd_golang.Add("1,4,-1")
+	fmt.Print(res)
+	t.Errorf("negatives not allowed: %d", res)
 }
